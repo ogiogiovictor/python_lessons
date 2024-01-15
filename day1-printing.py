87733bf31b2b2cdaca45434c9ff343efc55f2f41
Victor Ogiogio <victor.ogiogio@ibedc.com>
2024-01-15 19:12:04 +0100

    added new books

---
+++ day1-printing.py
@@ -0,0 +1,19 @@
+print("Welcome this the Day1 of Python 100 Days of Code Challenge!")
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')")
+
+
+##################################LINE BREAK ######################
+print("Adding a backslash\nHello World")
+
+######################## STRING CONCATENATION ####################
+print("Hello " + "Angela")
+
+############## INPUT FUNCTION #####################################
+input("What's your name? ")
+input("Hello " + input("Who are u? "))
+
+#len() tells you how many characters is in a particular string
+name = len(input("Len Function Tells me the Length of a String? "))
+print(name)
