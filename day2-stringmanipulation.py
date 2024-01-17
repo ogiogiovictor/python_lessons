431367f82fa2ad18f99b38852777653bd2e2d663
Victor Ogiogio <victor.ogiogio@ibedc.com>
2024-01-17 17:05:36 +0100

    added day 3

--- day2-stringmanipulation.py
+++ day2-stringmanipulation.py
@@ -59,3 +59,5 @@ people = int(input("How many people to split the bill?"))
 
 bill_with_tip = tip / 100 * bill + bill
 print(bill_with_tip)
+bill_per_person = round(bill_with_tip / people, 2)  #"{:2f}.format(bill_per_person)"
+print(f" Each person will pay {bill_per_person}")
